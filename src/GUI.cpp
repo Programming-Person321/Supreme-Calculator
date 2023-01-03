f491f6fc2e9f2da294631cd01c97219e8922abd7
Programming-Person321 <smile314@mymts.net>
2023-01-02 22:06:37 -0600

    Improved GUI and started calculations

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
+#include <vector>
 
 void load(Scene& scene, Button& b){
     int sceneToShow = stoi(b.getInstructions().substr(7, 1));
@@ -31,6 +33,32 @@ void appendText(Scene& scene, Button& b){
 
 void calculate(Scene& scene, Button& b){
     puts("Calculating...");
+
+
+    //if it has an exponent 
+    std::string input = scene.getTexts()[0].getText();
+
+    if(input.find("^") != std::string::npos){
+        puts("it has an exponent");
+            
+        printf("Index: %ld\n", input.find("^"));
+
+        int index = input.find("^");
+
+        //makes strings of before and after
+        std::string before = input.substr(0, (index));
+        printf("%s\n", before.c_str());
+
+        std::string after = input.substr((index + 1), input.length());
+        printf("%s\n", after.c_str());
+
+        //makes string to ints
+        int beforeInt = std::stoi(before);
+        int afterInt = std::stoi(after);
+
+        scene.getTexts()[0].clearText();
+        scene.getTexts()[0].addText(std::to_string(pow((double)beforeInt, (double)afterInt)));
+    }
 }
 
 void none(Scene& scene){}
@@ -47,10 +75,64 @@ void center(Scene& scene){
     if(c.getManagement())
         c.setPos(((width/2)-(c.getDim().x/2)) + c.getOriginalPos().x, (height/2)-(c.getDim().y/2) + c.getOriginalPos().y);
     }
+        
+    std::vector<int> sameX;
+    std::vector<int> sameY;
+    std::vector<int> pixelsX;
+    std::vector<int> pixelsY;
+
     for(Button& c : scene.getButtons()){
-    if(c.getManagement())
-        c.setPos(((width/2)-(c.getDim().x/2)) + c.getOriginalPos().x, (height/2)-(c.getDim().y/2) + c.getOriginalPos().y);
+        bool doesNotContainX = true;
+        bool doesNotContainY = true;
+
+        for(int i : pixelsX){
+            if (i == c.getPos().y) {doesNotContainX = false; break;}
+        }
+        for(int i : pixelsY){
+            if (i == c.getPos().x) {doesNotContainY = false; break;}
+        }
+
+        if(doesNotContainX){
+            pixelsX.push_back(c.getPos().y);
+            sameX.push_back(0);
+        }
+        if(doesNotContainY){
+            pixelsY.push_back(c.getPos().x);
+            sameY.push_back(0);
+        }
+
+        int indexX = 0;
+        while(true){
+            if (pixelsX[indexX] == c.getPos().y) break;
+            indexX++;
+        }
+        int indexY = 0;
+        while(true){
+            if (pixelsY[indexY] == c.getPos().x) break;
+            indexY++;
+        }
+
+        sameX[indexX]++;
+        sameY[indexY]++;
     }
+
+
+    for(int in : sameX){
+        printf("Printing Same X: %d\n", in);
+    }
+    for(int in : sameY){
+        printf("Printing Same Y: %d\n", in);
+    }
+
+    int topSpacer = 50;
+    int gridX = (width)/sameY.size();
+    int gridY = (height-topSpacer)/sameX.size();
+    for(Button& b : scene.getButtons()){
+        b.setPos((b.getOriginalPos().x+1) * gridX, ((b.getOriginalPos().y+1) * gridY) + topSpacer);
+        b.setDim(gridX, gridY);
+    }
+        
+
     for(TextBox& c : scene.getTexts()){
     if(c.getManagement()){
             c.setDim(width, c.getDim().y);
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -2,47 +2,5 @@
 #include "include/window.hpp"
 
 SDL_Texture* createBackground(){
-      int width, height;
-      SDL_GetRendererOutputSize(renderer, &width, &height);
-
-      SDL_Texture* image = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, width, height);
-      SDL_SetRenderTarget(renderer, image);
-
-      int ** noise = new int*[width];
-      for(int i = 0; i < width; i++){
-      noise[i] = new int[height];
-      }
-
-      for(int i = 0; i < width; i++){
-      for(int j = 0; j < height; j++){
-            bool x (i > 0);
-            bool y (j > 0);
-
-            if(x && y) {
-                  int avx = (noise[i-1][j] + noise[i][j-1])/2;
-                  noise[i][j] = {avx + ((rand()%3)-2)};
-            }else if(x){
-                  int avx = (noise[i-1][j]);
-                  noise[i][j] = {avx + ((rand()%3)-2)};
-            }else if(y){
-                  int avx = (noise[i][j-1]);
-                  noise[i][j] = {avx + ((rand()%3)-2)};
-            }else      {
-                  noise[i][j] = {(rand()%255)};
-            }
-
-            noise[i][j] = noise[i][j]%255;
-
-            SDL_SetRenderDrawColor(renderer, noise[i][j], noise[i][j], noise[i][j], 255);
-            SDL_RenderDrawPoint(renderer, i, j);
-      }
-      }
-
-      for(int i = width; i > 0;){
-            delete[] noise[--i];
-      }
-      delete[] noise;
-
-      SDL_SetRenderTarget(renderer, NULL);
-      return image;
+      return nullptr;
 }
